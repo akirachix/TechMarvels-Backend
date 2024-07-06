c26783b5b00b1fbf71749f3f2a9ed4e2db2b3755
Nancy-Nabacwa <nancypaulinenabacwa@gmail.com>
2024-07-06 21:05:01 +0300

    added postgress connection

--- web/Wezesha-App/Wezesha/wezeshaMobileApp/models.py
+++ web/Wezesha-App/Wezesha/wezeshaMobileApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Farmer(models.Model):
+    farmer_first_name = models.CharField(max_length = 20)
+    farmer_last_name = models.CharField(max_length = 20)
+    farmer_email= models.EmailField()
+    farmer_phone_number = models.CharField(max_length = 20)
+    farmer_loan_status = models.CharField(max_length = 20)
+    image = models.ImageField()
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
